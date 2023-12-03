2f4b06133a177009122a05f074bd6d051d63d99b
Akanimorex <akanimoime2016@gmail.com>
2023-12-03 02:12:02 +0100

    new error, yay progress

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,27 +1,34 @@
-import pkg from "./package.json"
+import babel from "@rollup/plugin-babel";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import { createRequire } from "node:module";
 
-
-import { createRequire } from 'node:module';
 const requireFile = createRequire(import.meta.url);
-const packageJson = requireFile('./package.json');
+const packageJson = requireFile("./package.json");
 
 export default {
-    input:'./index.js',
-    output: [
-        { file:pkg.main, format:"cjs" },
-        { file:pkg.module, format:"esm" }
-    ],
-    plugins: [
-        babel({
-          babelHelpers: 'bundled',
-          exclude: 'node_modules/**',
-          presets: ['@babel/preset-env','@babel/preset-react']
-        }),
-        resolve(),
-        commonjs(),
-        terser()
-      ],
-    external: Object.keys(pkg.peerDependencies)
-  };
+  input: "./components/index.js",
+  // external: ["react", "react-dom"],
+  output: [
+    {
+      file: packageJson.main,
+      format: "cjs",
+    },
+    {
+      file: packageJson.module,
+      format: "esm"
+    },
+  ],
+  plugins: [
+    babel({
+      exclude: "node_modules/**",
+      presets: ["@babel/preset-react"],
+      babelHelpers: "runtime",
+    }),
+    ,
+    resolve(),
+    commonjs(),
+    peerDepsExternal(),
+  ],
+};
