7af6ba4a51928382070a982f6a8546fc5d0fe6a3
Akanimorex <akanimoime2016@gmail.com>
2023-12-07 08:59:28 +0100

    bug fix

--- src/stories/pagination.stories.js
+++ src/stories/pagination.stories.js
@@ -13,7 +13,7 @@ export const sample =()=> {
     const recordsPerPageArray=[5,10,15,20]
 
 
-    return (<Pagination recordsPerPageArray={recordsPerPageArray} recordsPerPage={recordsPerPage} setRecordsPerPage={setRecordsPerPage}/>)
+    return (<Pagination dataArray={dataArray} recordsPerPageArray={recordsPerPageArray} recordsPerPage={recordsPerPage} setRecordsPerPage={setRecordsPerPage}/>)
 }
 
 //TODO: MAKE IT REUSABLE FOR DEVELOPERS, SHOULD BE ABLE TO PROVIDE AN ARRAY OF DATA FOR MANIPULATION, AND A NUMBER OF VALUES TO BE DISPLAYEDPER PAGE
