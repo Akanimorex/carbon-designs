13915d53c2c1891e368651c8a785fb623a921a17
Akanimorex <akanimoime2016@gmail.com>
2024-04-10 12:41:59 +0100

    commiting extra changes

--- index.js
+++ index.js
@@ -567,208 +567,6 @@ const Modal = ({
   }, "Cancel")))));
 };
 
-const Data = [{
-  "ID": 1,
-  "name": "John",
-  "email": "john@example.com"
-}, {
-  "ID": 2,
-  "name": "Doe",
-  "email": "doe@example.com"
-}, {
-  "ID": 3,
-  "name": "Alice",
-  "email": "alice@example.com"
-}, {
-  "ID": 4,
-  "name": "Bob",
-  "email": "bob@example.com"
-}, {
-  "ID": 5,
-  "name": "Eva",
-  "email": "eva@example.com"
-}, {
-  "ID": 6,
-  "name": "Charlie",
-  "email": "charlie@example.com"
-}, {
-  "ID": 7,
-  "name": "Sophie",
-  "email": "sophie@example.com"
-}, {
-  "ID": 8,
-  "name": "David",
-  "email": "david@example.com"
-}, {
-  "ID": 9,
-  "name": "Emma",
-  "email": "emma@example.com"
-}, {
-  "ID": 10,
-  "name": "Frank",
-  "email": "frank@example.com"
-}, {
-  "ID": 11,
-  "name": "Grace",
-  "email": "grace@example.com"
-}, {
-  "ID": 12,
-  "name": "Henry",
-  "email": "henry@example.com"
-}, {
-  "ID": 13,
-  "name": "Ivy",
-  "email": "ivy@example.com"
-}, {
-  "ID": 14,
-  "name": "Jack",
-  "email": "jack@example.com"
-}, {
-  "ID": 15,
-  "name": "Katie",
-  "email": "katie@example.com"
-}, {
-  "ID": 16,
-  "name": "Leo",
-  "email": "leo@example.com"
-}, {
-  "ID": 17,
-  "name": "Mia",
-  "email": "mia@example.com"
-}, {
-  "ID": 18,
-  "name": "Nathan",
-  "email": "nathan@example.com"
-}, {
-  "ID": 19,
-  "name": "Olivia",
-  "email": "olivia@example.com"
-}, {
-  "ID": 20,
-  "name": "Peter",
-  "email": "peter@example.com"
-}, {
-  "ID": 21,
-  "name": "Quinn",
-  "email": "quinn@example.com"
-}, {
-  "ID": 22,
-  "name": "Rachel",
-  "email": "rachel@example.com"
-}, {
-  "ID": 23,
-  "name": "Sam",
-  "email": "sam@example.com"
-}, {
-  "ID": 24,
-  "name": "Tom",
-  "email": "tom@example.com"
-}, {
-  "ID": 25,
-  "name": "Uma",
-  "email": "uma@example.com"
-}, {
-  "ID": 26,
-  "name": "Victor",
-  "email": "victor@example.com"
-}, {
-  "ID": 27,
-  "name": "Wendy",
-  "email": "wendy@example.com"
-}, {
-  "ID": 28,
-  "name": "Xander",
-  "email": "xander@example.com"
-}, {
-  "ID": 29,
-  "name": "Yara",
-  "email": "yara@example.com"
-}, {
-  "ID": 30,
-  "name": "Zane",
-  "email": "zane@example.com"
-}, {
-  "ID": 31,
-  "name": "Abby",
-  "email": "abby@example.com"
-}, {
-  "ID": 32,
-  "name": "Ben",
-  "email": "ben@example.com"
-}, {
-  "ID": 33,
-  "name": "Cara",
-  "email": "cara@example.com"
-}, {
-  "ID": 34,
-  "name": "Dylan",
-  "email": "dylan@example.com"
-}, {
-  "ID": 35,
-  "name": "Ella",
-  "email": "ella@example.com"
-}, {
-  "ID": 36,
-  "name": "Finn",
-  "email": "finn@example.com"
-}, {
-  "ID": 37,
-  "name": "Gina",
-  "email": "gina@example.com"
-}, {
-  "ID": 38,
-  "name": "Harry",
-  "email": "harry@example.com"
-}, {
-  "ID": 39,
-  "name": "Isabel",
-  "email": "isabel@example.com"
-}, {
-  "ID": 40,
-  "name": "Jake",
-  "email": "jake@example.com"
-}, {
-  "ID": 41,
-  "name": "Kara",
-  "email": "kara@example.com"
-}, {
-  "ID": 42,
-  "name": "Liam",
-  "email": "liam@example.com"
-}, {
-  "ID": 43,
-  "name": "Megan",
-  "email": "megan@example.com"
-}, {
-  "ID": 44,
-  "name": "Nolan",
-  "email": "nolan@example.com"
-}, {
-  "ID": 45,
-  "name": "Oliver",
-  "email": "oliver@example.com"
-}, {
-  "ID": 46,
-  "name": "Penny",
-  "email": "penny@example.com"
-}, {
-  "ID": 47,
-  "name": "Quincy",
-  "email": "quincy@example.com"
-}, {
-  "ID": 48,
-  "name": "Ruby",
-  "email": "ruby@example.com"
-}, {
-  "ID": 49,
-  "name": "Seth",
-  "email": "seth@example.com"
-}, {
-  "ID": 50,
-  "name": "Tara",
-  "email": "tara@example.com"
-}];
-
 function styleInject(css, ref) {
   if ( ref === void 0 ) ref = {};
   var insertAt = ref.insertAt;
@@ -800,34 +598,22 @@ var css_248z = ".pagination{list-style-type:none}.pagination-item{display:inline
 styleInject(css_248z,{"insertAt":"top"});
 
 const Pagination = ({
-  recordsPerPageArray
+  recordsPerPageArray,
+  dataArray,
+  recordsPerPage,
+  setRecordsPerPage
 }) => {
-  console.log(recordsPerPageArray, "records-array");
-
-  // TODO: upgrade this
-  // no. of items to be allowed items per page (select input style)
-  //total item count
-  //no of pages left
-
-  //top of the list of each page
-  //bottom of the list of each page
-
   const [currentPage, setCurrentPage] = React.useState(1);
   const [isPrevDisabled, setPrevDisabled] = React.useState(false);
   const [isNextDisabled, setNextDisabled] = React.useState(false);
-  const [recordsPerPage, setRecordsPerPage] = React.useState(5);
-  const totalNoOfItems = Data.length;
-  // const recordsPerPage = 5; // i'll need this
+  // const [recordsPerPage, setRecordsPerPage] = useState(5)
+  const totalNoOfItems = dataArray ? dataArray.length : 0;
   const lastIndex = currentPage * recordsPerPage;
   const firstIndex = lastIndex - recordsPerPage;
-  const records = Data.slice(firstIndex, lastIndex);
-  const nPage = Math.ceil(Data.length / recordsPerPage);
+  const records = dataArray ? dataArray.slice(firstIndex, lastIndex) : [];
+  const nPage = dataArray ? Math.ceil(dataArray.length / recordsPerPage) : 0;
   const numbers = [...Array(nPage + 1).keys()].slice(1);
   React.useEffect(() => {
-    console.log(lastIndex, "last index");
-    console.log(firstIndex, "first index");
-    console.log(currentPage, "currentPage");
-
     // let lastPage = Data.length-1;
     if (currentPage <= 1) {
       setPrevDisabled(true);
@@ -870,23 +656,9 @@ const Pagination = ({
     className: "flex min-h-screen items-center justify-center"
   }, /*#__PURE__*/React.createElement("div", {
     className: "w-full"
-  }, /*#__PURE__*/React.createElement("table", {
-    className: "w-1/2  bg-gray-500 leading-normal"
-  }, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", {
-    className: "border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-700"
-  }, "ID"), /*#__PURE__*/React.createElement("th", {
-    className: "border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-700"
-  }, "Name"), /*#__PURE__*/React.createElement("th", {
-    className: "border-b-2 border-gray-200 bg-gray-100 px-2 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-700"
-  }, "Email"))), /*#__PURE__*/React.createElement("tbody", null, records.map((d, i) => /*#__PURE__*/React.createElement("tr", {
+  }, records > 0 ? records.map((d, i) => /*#__PURE__*/React.createElement("div", {
     key: i
-  }, /*#__PURE__*/React.createElement("td", {
-    className: "border-b border-gray-200 bg-white px-3 py-3 text-sm"
-  }, d.ID), /*#__PURE__*/React.createElement("td", {
-    className: "border-b border-gray-200 bg-white px-3 py-3 text-sm"
-  }, d.name), /*#__PURE__*/React.createElement("td", {
-    className: "border-b border-gray-200 bg-white px-3 py-3 text-sm"
-  }, d.email))))), /*#__PURE__*/React.createElement("nav", {
+  }, d.title)) : null, /*#__PURE__*/React.createElement("nav", {
     className: "flex w-1/2 bg-gray-300"
   }, /*#__PURE__*/React.createElement("select", {
     name: "",
